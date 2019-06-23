b544fb6a4e7f02c631b20a1ec480498943c15e49
Kavyashreekt <52081275+Kavyashreekt@users.noreply.github.com>
2019-06-23 09:15:54 +0530

    Added swap functionality, also changed the starting index of j

--- BubbleSort/BubbleSort/BubbleSort.c
+++ BubbleSort/BubbleSort/BubbleSort.c
@@ -6,11 +6,15 @@ void bubble_sort(int data[], int size)
 {
 	int i;
 	int j;
+	int temp;
 
 	for (i = 0; i < size; i++) {
-		for (j = 0; j < size; j++) {
+		for (j = i+1; j < size; j++) {
 			if (data[i] < data[j]) {
-				swap(data + i, data + j);
+				//swap(data + i, data + j);
+				temp=data[i];
+				data[i]=data[j];
+				data[j]=temp;
 			}
 		}
 	}
